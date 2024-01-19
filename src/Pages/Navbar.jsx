05aa3f5c0a86f4ed89493e78e05ee10b073c5a74
Andy Tran <AyyyTran@gmail.com>
2024-01-18 21:03:33 -0800

    Edited Logo div so it is centered

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <navbar className="fixed top-0 z-50 flex items-center justify-between px-4 text-lg lg:text-xl w-full h-16 bg-secondary text-white lg:h-20">
       <Link
-        className="w-40 text-3xl font-bold text-white hover:cursor-pointer "
+        className="w-40 text-3xl font-bold text-white hover:cursor-pointer flex items-center justify-center "
         to="hero"
         spy={true}
         smooth={true}
