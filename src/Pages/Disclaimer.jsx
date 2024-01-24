88a00ce1d7fddcb373af26397cae85b402acf1fc
Andy Tran <AyyyTran@gmail.com>
2024-01-23 20:38:52 -0800

    Edited x axis margin and width of disclaimer

--- src/Pages/Disclaimer.jsx
+++ src/Pages/Disclaimer.jsx
@@ -4,7 +4,7 @@ const Disclaimer = () => {
   return (
     <div className="flex-col justify-center items-center py-8">
       <div className="text-2xl font-bold py-2">Medical Disclaimer:</div>
-      <div className="text-l text-center lg:mx-auto lg:max-w-4xl">
+      <div className="text-l text-center mx-auto w-5/6 lg:max-w-4xl">
         The material and information provided is for informational purposes
         only. It is not intended as a practice of veterinary medicine or the
         provision of veterinary medical services. This information is not
