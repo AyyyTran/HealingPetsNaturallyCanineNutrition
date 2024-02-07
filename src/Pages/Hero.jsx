604d1ae5a67ae17c809489413acff87c99a66552
Andy Tran <AyyyTran@gmail.com>
2024-02-06 19:08:10 -0800

    added hero bg image.

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div className="flex flex-col mx-auto w-96 pt-20 lg:pt-24 pb-4 h-screen justify-center">
-      <h1 className="text-3xl">
-        Healthier & Happier Dogs with Premium Nutrition
-      </h1>
-      <h2 className="text-2xl py-4">
-        Discover Nutritious Diets Tailored for Your Furry Friend's Well-being.
-      </h2>
-      <button className="bg-grey rounded-xl p-4 w-32 mx-auto text-l">
-        Book Now
-      </button>
+    <div className=" bg-hero bg-cover bg-center flex flex-col mx-auto w-full pb-4 h-screen">
+      <div className="shadow-sm ">
+        <div className="w-96 justify-center mx-auto pt-48">
+          <h1 className="text-3xl text-white [text-shadow:_0_1px_0_rgb(0_0_0_/_100%)]">
+            Healthier & Happier Dogs with Premium Nutrition
+          </h1>
+        </div>
+        <div className="w-96 justify-center mx-auto">
+          <h2 className="text-2xl text-white [text-shadow:_0_1px_0_rgb(0_0_0_/_100%)] py-4">
+            Discover Nutritious Diets Tailored for Your Furry Friend's
+            Well-being.
+          </h2>
+        </div>
+      </div>
+      <div className="w-96 justify-center mx-auto">
+        <button className="bg-primary text-white text-xl rounded-xl p-4 w-32 mx-auto text-l lg:hover:-translate-y-0.5 lg:hover:scale-105 lg:hover:bg-white lg:hover:text-primary">
+          Book Now
+        </button>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('../public/ChowChow.jpg')",
+      },
+    },
     colors: {
       primary: '#6D9DC5',
       secondary: '#467FAF',
       accent: '#99BAD6',
-      white: '#FFFFFF',
+      white: '#FAF9F6',
       dark: '#48494B',
       grey: '#F5F5F5',
       darkblue: '#102542',
