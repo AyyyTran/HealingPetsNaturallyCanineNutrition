1a517b0d5b71586d3e02cdd34e319483cf11f7ad
Andy Tran <AyyyTran@gmail.com>
2024-02-07 20:17:03 -0800

    Created express node server.js backend. Added nodemailer for basic send email

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+// backend/server.js
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ayyytran@gmail.com',
+    pass: 'uzrj olea dkgd uecn',
+  },
+});
+
+app.get('/send-email', async (req, res) => {
+  try {
+    const mailOptions = {
+      from: 'ayyytran@gmail.com',
+      to: 'ayyytran@gmail.com',
+      subject: 'Test Email',
+      text: 'This is a test email.',
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.send('Email sent successfully!');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(`Error sending email: ${error.message}`);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
