fdefb4671027d71da2cd153a32f0ebb274c26971
Andy Tran <AyyyTran@gmail.com>
2024-02-07 18:48:58 -0800

    Added placeholders and required attribute to inputs

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -15,6 +15,8 @@ const Contact = () => {
             id="name"
             name="name"
             className="rounded-xl text-darkblue outline-none p-2"
+            required
+            placeholder="John Doe"
           />
         </div>
         <div className="flex flex-col w-4/5 mx-auto">
@@ -26,6 +28,8 @@ const Contact = () => {
             id="address"
             name="address"
             className="rounded-xl text-darkblue outline-none p-2"
+            required
+            placeholder="123 Main St, City"
           />
         </div>
         <div className="flex flex-col w-4/5 mx-auto">
@@ -37,6 +41,8 @@ const Contact = () => {
             id="email"
             name="email"
             className="rounded-xl text-darkblue outline-none p-2"
+            required
+            placeholder="john.doe@example.com"
           />
         </div>
         <div className="flex flex-col w-4/5 mx-auto">
@@ -44,10 +50,13 @@ const Contact = () => {
             Phone:
           </label>
           <input
-            type="text"
+            type="tel"
             id="phone"
             name="phone"
             className="rounded-xl text-darkblue outline-none p-2"
+            required
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+            placeholder="123-456-7890"
           />
         </div>
 
@@ -61,6 +70,8 @@ const Contact = () => {
             id="pname"
             name="pname"
             className="rounded-xl text-darkblue outline-none p-2"
+            required
+            placeholder="Rusty"
           />
         </div>
         <div className="flex flex-col w-4/5 mx-auto">
@@ -72,6 +83,8 @@ const Contact = () => {
             id="breed"
             name="breed"
             className="rounded-xl text-darkblue outline-none p-2"
+            required
+            placeholder="Labrador"
           />
         </div>
         <div className="flex flex-col w-4/5 mx-auto">
@@ -85,6 +98,8 @@ const Contact = () => {
             cols="30"
             rows="10"
             className="rounded-xl text-darkblue outline-none p-2"
+            required
+            placeholder="Ex: Allergic to fish, chicken and has troubles digesting seeds. Currently being fed dog food and no supplements."
           ></textarea>
         </div>
         <div className="flex flex-col w-4/5 mx-auto">
@@ -98,19 +113,8 @@ const Contact = () => {
             cols="30"
             rows="10"
             className="rounded-xl text-darkblue outline-none p-2"
-          ></textarea>
-        </div>
-        <div className="flex flex-col w-4/5 mx-auto">
-          <label for="weight" className="text-start px-2 py-4">
-            Please describe how active your pet is. What is the current weight
-            and ideal weight of your pet. How much food do you feed it?
-          </label>
-          <textarea
-            name="weight"
-            id="weight"
-            cols="30"
-            rows="10"
-            className="rounded-xl text-darkblue outline-none p-2"
+            required
+            placeholder="Ex: Rusty is not very active. Only an hour of outside time daily. Currently weighs 150lbs but want him to be a healthier weight. Currently fed 3 meals a day plus dog treats and dental stick."
           ></textarea>
         </div>
         <div className="w-4/5 mx-auto pt-4">
