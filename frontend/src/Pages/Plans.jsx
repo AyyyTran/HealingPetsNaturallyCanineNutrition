7e6aa3896a57a77b3e361162cc9c115d3e269293
Andy Tran <AyyyTran@gmail.com>
2024-02-14 19:43:19 -0800

    Edited plans order and confirmation of services

--- frontend/src/Pages/Plans.jsx
+++ frontend/src/Pages/Plans.jsx
@@ -23,37 +23,6 @@ const Plans = () => {
 
         <div className="bg-grey rounded-xl p-4 w-3/4 md:w-4/5 mx-auto my-2 flex flex-col justify-between shadow-xl lg:transition lg:ease-in-out lg:hover:-translate-y-0.5 lg:hover:scale-10">
           <h2 className="text-xl font-semibold text-darkblue">
-            Premium Consultation Plan
-          </h2>
-          <ul className="w-2/3 md:w-4/5 mx-auto list-disc py-4">
-            <li>
-              Going over your dog&apos;s health history, current, and past food
-              as well as supplements
-            </li>
-            <li>
-              Discuss the benefits of raw and fresh food and what you are open
-              to feeding
-            </li>
-            <li>Customized food and supplement plan with weekly follow-ups</li>
-            <li>
-              Weekly follow-ups for 12 weeks to make sure everything is going
-              well and to see if anything new should be added or removed
-            </li>
-            <li>
-              Free bone broth recipe, which is great for joint, immune, gut
-              health, and many more
-            </li>
-            <li>
-              Discuss any concerns or questions regarding food, supplements, or
-              other health questions
-            </li>
-            <li>Zoom or telephone consultations</li>
-          </ul>
-          <div className="font-bold py-4 text-xl">$130 CAD</div>
-        </div>
-
-        <div className="bg-grey rounded-xl p-4 w-3/4 md:w-4/5 mx-auto my-2 flex flex-col justify-between shadow-xl lg:transition lg:ease-in-out lg:hover:-translate-y-0.5 lg:hover:scale-10">
-          <h2 className="text-xl font-semibold text-darkblue">
             Nutrition Plan
           </h2>
           <ul className="w-2/3 md:w-4/5 mx-auto list-disc py-4">
@@ -81,6 +50,33 @@ const Plans = () => {
             <li>Zoom or telephone consultations</li>
           </ul>
           <div className="font-bold py-4 text-xl">$75 CAD</div>
+        </div>
+
+        <div className="bg-grey rounded-xl p-4 w-3/4 md:w-4/5 mx-auto my-2 flex flex-col justify-between shadow-xl lg:transition lg:ease-in-out lg:hover:-translate-y-0.5 lg:hover:scale-10">
+          <h2 className="text-xl font-semibold text-darkblue">
+            Premium Consultation Plan
+          </h2>
+          <ul className="w-2/3 md:w-4/5 mx-auto list-disc py-4">
+            <li>
+              Going over your dog&apos;s health history, current, and past food
+              as well as supplements
+            </li>
+            <li>
+              Discuss the benefits of raw and fresh food and what you are open
+              to feeding
+            </li>
+            <li>Customized food and supplement plan with weekly follow-ups</li>
+            <li>
+              Weekly follow-ups for 12 weeks to make sure everything is going
+              well and to see if anything new should be added or removed
+            </li>
+            <li>
+              Discuss any concerns or questions regarding food, supplements, or
+              other health questions
+            </li>
+            <li>Zoom or telephone consultations</li>
+          </ul>
+          <div className="font-bold py-4 text-xl">$130 CAD</div>
         </div>
       </div>
     </div>
