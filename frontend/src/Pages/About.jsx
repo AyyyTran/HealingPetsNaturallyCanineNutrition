19a38368a55ea6e172468272996231fdc46cf052
Andy Tran <AyyyTran@gmail.com>
2024-02-19 20:42:48 -0800

    changed content of my story

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -32,31 +32,45 @@ const About = () => {
       <div className="flex flex-col items-center py-4 lg:w-4/5 lg:mx-auto">
         <h2 className="text-3xl font-medium py-4">My Story</h2>
         <p className="px-8 py-4 lg:text-l">
-          I have always had a love for animals and when I bought my first dog, a
-          French Bulldog, I knew they were prone to many health issues as I did
-          my research before hand. With all the research I had done before, I
-          was still in for a shock with everything my little guy had and no one
-          could fully diagnose the issues.
+          Since my childhood, I have harbored a deep love for animals. When I
+          welcomed my first pet, a French Bulldog, into my life, I was well
+          aware of the breed's susceptibility to various health issues. Despite
+          my thorough research beforehand, I was taken aback by the multitude of
+          ailments my furry companion, whom I affectionately named Rusty, began
+          to experience.
+        </p>
+        <p className="px-8 py-4 lg:text-l">
+          Rusty was subjected to numerous rounds of antibiotics, each
+          exacerbating his condition and leading to a myriad of health concerns
+          such as yeast infections, leaky gut, skin problems, paw licking,
+          warts, hives, urine crystals, and a compromised immune system.
+          Frustrated by the lack of a definitive diagnosis and the ineffective
+          conventional treatments, I delved into the realm of natural remedies
+          and holistic healing methods.
         </p>
         <img
           src="/VestPug.jpg"
           alt="VestPug"
-          className="max-w-72 max-h-96 rounded-xl"
+          className="max-w-72 max-h-96 rounded-xl my-4"
         />
-        <p className="px-8 py-4 lg:text-l">
-          He was put on antibiotics over and over again each time causing him
-          more issues. Yeast issues, leaky gut, poor skin coat, licking paws,
-          warts, hives/rashes, urine crystals and a weakened immune system. I
-          started to do research and was amazed at all the natural remedies out
-          there that work much better than antibiotics and steroids. I started
-          him on fresh foods and supplements and the difference was incredible.
-          I started to notice some improvements in just a couple weeks.
+        <p className="px-8 py-2 lg:text-l">
+          Through extensive research and experimentation, I discovered the
+          profound impact of fresh, wholesome foods and supplements on Rusty’s
+          well-being. Witnessing remarkable improvements in his health within a
+          mere few weeks, I was inspired to delve deeper into canine nutrition.
+          Enrolling in specialized courses, I embarked on a journey to educate
+          myself about the transformative power of proper nutrition in healing
+          and preventing various health issues in pets.
         </p>
         <p className="px-8 py-2 lg:text-l">
-          With all the research I had done I started to take canine nutrition
-          courses and realized I could help many other pet parents heal their
-          dogs without toxins and chemicals. I now know that many of the health
-          issues we deal with can be prevented with proper nutrition.
+          Empowered by this newfound knowledge and personal experience, I became
+          determined to assist fellow pet parents in nurturing and healing their
+          beloved companions through natural, toxin-free methods. I came to
+          realize that many of the health challenges faced by our furry friends
+          can be mitigated and even avoided through a holistic approach to
+          nutrition and well-being. Today, I am passionate about sharing this
+          valuable insight with others, striving to make a positive difference
+          in the lives of pets and their devoted caregivers.
         </p>
       </div>
 
