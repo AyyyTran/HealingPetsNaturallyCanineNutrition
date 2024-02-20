6bb4ee8572feceea6a95c4b99f0ec244e504ad1b
Andy Tran <AyyyTran@gmail.com>
2024-02-19 20:46:12 -0800

    Explore services and contact me links set

--- frontend/src/Pages/Info.jsx
+++ frontend/src/Pages/Info.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link} from 'react-scroll';
 
 const Info = () => {
   return (
     <div className="bg-grey py-4">
-      <h2 className="text-xl text-secondary ">
+      <h2 className="text-2xl text-secondary ">
         The Real Culprit: Low-Quality Pet Food
       </h2>
-      <p className="text-sm p-4 mx-auto lg:w-1/2">
+      <p className=" p-4 mx-auto lg:w-1/2 ">
         Many health issues stem from low-quality pet food. Pet food companies
         prioritize marketing and profit over your pet's health. They can include
         undisclosed ingredients, chemicals, and even garbage in their products,
@@ -18,9 +19,29 @@ const Info = () => {
         — promoting longevity, vitality, and overall well-being.
       </p>
       <div className="bg-primary w-4/5 md:w3/4 lg:w-1/2 mx-auto rounded-xl p-4 text-grey">
-        Ready to embark on a journey to better health for your furry friend?
-        <a href="/"> Explore Services </a> or <a href="/">Contact Me</a> for a
-        consultation.
+        Ready to embark on a journey to better health for your furry friend?{' '}
+        <Link
+          to="services"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="underline px-1 hover:text-secondary"
+        >
+          Explore Services
+        </Link>{' '}
+        or{' '}
+        <Link
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="underline px-1 hover:text-secondary"
+        >
+          Contact Me
+        </Link>{' '}
+        for a consultation.
       </div>
     </div>
   );
