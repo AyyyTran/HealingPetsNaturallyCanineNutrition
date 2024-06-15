0d6ad1f137ecb2b1c1c052ee105103df179c11ec
Andy Tran <AyyyTran@gmail.com>
2024-06-15 15:57:46 -0700

    Fixed disabling correct unavailable dates

--- frontend/src/Pages/DateTimeValidation.jsx
+++ frontend/src/Pages/DateTimeValidation.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import TextField from '@mui/material/TextField';
 import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
 import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 import {DateTimePicker} from '@mui/x-date-pickers/DateTimePicker';
 import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const DateTimeValidation = ({unavailableDates, name, value, onChange}) => {
-  const parsedValue = value ? dayjs(value) : null;
+  const parsedValue = value ? dayjs(value).utc() : null;
 
   const handleChange = (newValue) => {
-    onChange(name, newValue ? newValue.toString() : null);
+    onChange(name, newValue ? newValue.toISOString() : null);
   };
 
   const shouldDisableDate = (date) => {
-    return unavailableDates.includes(date.format('YYYY-MM-DD'));
+    const formattedDate = date.utc().format('YYYY-MM-DD');
+    const isDisabled = unavailableDates.some((unavailableDate) => {
+      return (
+        formattedDate === dayjs(unavailableDate).utc().format('YYYY-MM-DD')
+      );
+    });
+    console.log(`Date ${formattedDate} is disabled:`, isDisabled);
+    return isDisabled;
   };
+
+  useEffect(() => {
+    console.log('DateTimeValidation component mounted');
+    console.log('unavailableDates:', unavailableDates);
+    console.log('parsedValue:', parsedValue);
+  }, [unavailableDates, parsedValue]);
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -29,7 +47,7 @@ const DateTimeValidation = ({unavailableDates, name, value, onChange}) => {
         label="Choose Appointment Date & Time"
         value={parsedValue}
         onChange={handleChange}
-        minDate={dayjs()}
+        minDate={dayjs().utc()}
         minTime={dayjs().startOf('day').add(9, 'hour')}
         maxTime={dayjs().startOf('day').add(17, 'hour')}
         minutesStep={60}
